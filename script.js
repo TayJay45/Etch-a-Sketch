bd6a09cc6721b8b49e4ba873ea73821cd0dfebf3
Taylor Jennings <taylor.jennings128@gmail.com>
2023-06-12 22:38:50 -0600

    set up div grid

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("what up");
+
+let mainDiv = document.querySelector(".main-div");
+
+for (i = 0; i < 17; i++) {
+  let div = document.createElement("div");
+  mainDiv.appendChild(div);
+}
