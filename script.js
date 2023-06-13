8d500eec172159fcb414dd84384dee4525f9ec9b
Taylor Jennings <taylor.jennings128@gmail.com>
2023-06-13 13:48:43 -0600

    built grid

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-console.log("what up");
+let grid = document.querySelector(".grid");
 
-let mainDiv = document.querySelector(".main-div");
-
-for (i = 0; i < 17; i++) {
+// build grid
+for (i = 0; i < 256; i++) {
   let div = document.createElement("div");
-  mainDiv.appendChild(div);
+  div.classList.add("cell");
+  div.addEventListener("mouseover", function (event) {
+    event.target.style.backgroundColor = "black";
+  });
+  grid.appendChild(div);
+}
+
+function newGrid() {
+  let gridSize = prompt("Please enter grid size between 16 and 100");
+  resetGrid(gridSize);
+}
+
+function resetGrid(gridSize) {
+  for (i = 0; i < gridSize; i++) {
+    let div = document.createElement("div");
+    div.classList.add("cell");
+    div.addEventListener("mouseover", function (event) {
+      event.target.style.backgroundColor = "black";
+    });
+    grid.appendChild(div);
+  }
 }
